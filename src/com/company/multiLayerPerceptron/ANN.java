c65f354d0450f81429ba642f62977a2200aaa374
Felipe Lodes <felipelodes@hotmail.com>
2020-04-07 01:15:33 -0300

    implement nw generator instances

--- src/com/company/multiLayerPerceptron/ANN.java
+++ src/com/company/multiLayerPerceptron/ANN.java
@@ -1,5 +1,7 @@
 package com.company.multiLayerPerceptron;
 
+import com.company.utils.generator.MatrixGenerator;
+import com.company.utils.generator.VectorGenerator;
 import com.company.utils.math.Function;
 
 /**
@@ -24,7 +26,20 @@ public class ANN {
     private int currentEpoch;
     //private double meanError;
 
+    private Double[][] hiddenWeightMatrix;
+    private Double[][] outputWeightMatrix;
+
     Function<Double, Double> activationFunction;
+
+    private Double inputXVector[];
+    private Double expectedYvector[];
+
+    private final double bias = 1;
+
+
+    public void start() {
+
+    }
 
     public ANN(int inputLayerNeuronNumber,
                int outputLayerNeuronNumber,
@@ -40,9 +55,73 @@ public class ANN {
         this.epochMaxNumber = epochMaxNumber;
         this.activationFunction = activationFunction;
         this.currentEpoch = 0;
+
+        hiddenWeightMatrix = new Double[inputLayerNeuronNumber + 1][hiddenLayerNeuronNumber];
+        outputWeightMatrix = new Double[hiddenLayerNeuronNumber + 1][outputLayerNeuronNumber];
+
+        inputXVector = new Double[inputLayerNeuronNumber + 1];
+        expectedYvector = new Double[outputLayerNeuronNumber];
+
+        VectorGenerator vectorGenerator = new VectorGenerator();
+        inputXVector = vectorGenerator.generate(inputXVector);
+        expectedYvector = vectorGenerator.generate(expectedYvector);
+
+        inputXVector[0] = bias;
+
+        MatrixGenerator generator = new MatrixGenerator();
+        hiddenWeightMatrix = generator.generate(hiddenWeightMatrix);
+        outputWeightMatrix = generator.generate(outputWeightMatrix);
     }
 
-    public void start() {
+    public ANN(int inputLayerNeuronNumber,
+               int outputLayerNeuronNumber,
+               int hiddenLayerNeuronNumber,
+               double learningRate,
+               int epochMaxNumber,
+               Function<Double, Double> activationFunction,
+               Double[] inputXVector,
+               Double[] expectedYvector) {
+        this.inputLayerNeuronNumber = inputLayerNeuronNumber;
+        this.outputLayerNeuronNumber = outputLayerNeuronNumber;
+        this.hiddenLayerNeuronNumber = hiddenLayerNeuronNumber;
+        this.learningRate = learningRate;
+        this.errorRate = Double.POSITIVE_INFINITY;
+        this.epochMaxNumber = epochMaxNumber;
+        this.activationFunction = activationFunction;
+        this.currentEpoch = 0;
+        this.inputXVector = inputXVector;
+        this.expectedYvector = expectedYvector;
 
+        hiddenWeightMatrix = new Double[inputLayerNeuronNumber + 1][hiddenLayerNeuronNumber];
+        outputWeightMatrix = new Double[hiddenLayerNeuronNumber + 1][outputLayerNeuronNumber];
+
+        MatrixGenerator generator = new MatrixGenerator();
+        hiddenWeightMatrix = generator.generate(hiddenWeightMatrix);
+        outputWeightMatrix = generator.generate(outputWeightMatrix);
+    }
+
+    public ANN(int inputLayerNeuronNumber,
+               int outputLayerNeuronNumber,
+               int hiddenLayerNeuronNumber,
+               double learningRate,
+               int epochMaxNumber,
+               Function<Double, Double> activationFunction,
+               Double[] inputXVector,
+               Double[] expectedYvector,
+               Double[][] hiddenWeightMatrix,
+               Double[][] outputWeightMatrix) {
+        this.inputLayerNeuronNumber = inputLayerNeuronNumber;
+        this.outputLayerNeuronNumber = outputLayerNeuronNumber;
+        this.hiddenLayerNeuronNumber = hiddenLayerNeuronNumber;
+        this.learningRate = learningRate;
+        this.errorRate = Double.POSITIVE_INFINITY;
+        this.epochMaxNumber = epochMaxNumber;
+        this.activationFunction = activationFunction;
+        this.currentEpoch = 0;
+        this.inputXVector = inputXVector;
+        this.expectedYvector = expectedYvector;
+
+        this.hiddenWeightMatrix = hiddenWeightMatrix;
+        this.outputWeightMatrix = outputWeightMatrix;
     }
 }
--- src/com/company/utils/generator/RandomGenerator.java
+++ src/com/company/utils/generator/RandomGenerator.java
@@ -1,5 +1,7 @@
 package com.company.utils.generator;
 
+import java.util.Random;
+
 /**
  Created by: Felipe Lodes in 07/04/2020.
  Discipline: ACH2016 - "Inteligência Artificial"
@@ -9,6 +11,11 @@ package com.company.utils.generator;
 
  **/
 
-public interface RandomGenerator<E> {
-    E generate();
+public interface RandomGenerator<E, T> {
+
+    E generate(T value);
+
+    default Double generateDouble() {
+        return new Random().nextDouble();
+    }
 }
