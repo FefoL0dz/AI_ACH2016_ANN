aa5d3d790b6b4ffeab50ef889b33be3b46f73fd7
flodes <felipelodes@hotmail.com>
2020-04-07 23:08:12 -0300

    add NotYetImplemented and MainIFunction signatures

--- src/com/company/math/test/MainFunctionTest.java
+++ src/com/company/math/test/MainFunctionTest.java
@@ -3,6 +3,15 @@ package com.company.math.test;
 import com.company.math.IFunction;
 import com.company.math.Sigmoid;
 
+/**
+ Created by: Felipe Lodes in 07/04/2020.
+ Discipline: ACH2016 - "Inteligência Artificial"
+ Professor: Sarajane
+
+ *Copyright (c) 2020 FelipeLodes to Present. All rights reserved.*
+
+ **/
+
 public class MainFunctionTest {
     public static void main(String[] args) {
         IFunction<Double, Double> function;
--- src/com/company/utils/exception/NotYetImplementedException.java
+++ src/com/company/utils/exception/NotYetImplementedException.java
@@ -1,5 +1,14 @@
 package com.company.utils.exception;
 
+/**
+ Created by: Felipe Lodes in 07/04/2020.
+ Discipline: ACH2016 - "Inteligência Artificial"
+ Professor: Sarajane
+
+ *Copyright (c) 2020 FelipeLodes to Present. All rights reserved.*
+
+ **/
+
 public class NotYetImplementedException extends RuntimeException {
     private final static String message = "Not yet implemented!";
     public NotYetImplementedException() {
