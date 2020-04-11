28d2fa15a5848cde5ad4618190dd61c008637404
bryan-via <bryan.munekata@viaconsulting.com.br>
2020-04-11 19:26:14 -0300

    added InputTester.java

---
+++ src/com/company/tools/IO/input/MainInputTester.java
@@ -0,0 +1,21 @@
+package com.company.tools.IO.input;
+
+import java.io.FileNotFoundException;
+import com.company.tools.IO.input.InputReader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MainInputTester {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputReader inputReader = new InputReader("com/company/tools/IO/input/inputFiles/problemAND", ".csv");
+        List<List<Double>> input = inputReader.read();
+
+        Iterator iterator = input.iterator();
+
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
