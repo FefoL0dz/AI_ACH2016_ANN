7fd6dcb5971b88e618a71fa46903ddd60a1dead6
flodes <felipelodes@hotmail.com>
2020-04-13 01:29:14 -0300

    Remove Windows User Hard Coded

--- src/com/company/tools/IO/FileURIComponents.java
+++ src/com/company/tools/IO/FileURIComponents.java
@@ -10,7 +10,7 @@ public interface FileURIComponents {
     String USERS = "Users";
 
     //TODO: Improve this process getting project path automatically
-    String PROFILE_USER = "Felipe Lodes";
+    String PROFILE_USER = "(YOUR_USER_HERE)";
 
     String DESKTOP = "Desktop";
     String PROJECT_NAME = "AI_ACH2016_ANN";
